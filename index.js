7ec7c52ea06a93ec0e759f6c0e81b3644676921f
sdpercy <scottpercy@hotmail.com>
2021-07-05 10:55:16 -0230

    added view all employees

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ const viewEmployees = () => {
     return new Promise((res, reject)=>{
     console.log("Viewing all employees\n");
 
-    const sqlQuery = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS Department, role.salary, employee.
+    const sqlQuery = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS Department, role.salary,
                         CONCAT (manager.first_name, " ", manager.last_name) AS Manager
                         FROM employee
                         LEFT JOIN role ON employee.role_id = role.id
