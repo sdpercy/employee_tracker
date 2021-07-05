846799db3ccfbaf72092eb75f22cdc402f9eb01c
sdpercy <scottpercy@hotmail.com>
2021-07-05 10:34:11 -0230

    added view department

--- index.js
+++ index.js
@@ -1,92 +1,99 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 const consoleTable = require('console.table');
 
-async function connect() {
-        const con = await mysql.createConnection({
-                host: 'localhost',
-                // Your MySQL username,
-                user: 'root',
-                // Your MySQL password
-                password: 'D0lph1ns1399$',
-                database: 'employeeDB'
-        });
-        
-        con.connect(function (err, con) {
-            if (err) throw err;
-            console.log('Connected to the employeeDB database.');
-        })
 
-        console.table(`
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'D0lph1ns1399$',
+        database: 'employeeDB'
+    },
+    console.log('Connected to the Employee Database.'),
+    );
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    console.table(`
  =================================================== 
-╔═══╗     ╔╗               ╔═╗╔═╗
-║╔══╝     ║║               ║║╚╝║║
-║╚══╦╗╔╦══╣║╔══╦╗ ╔╦══╦══╗ ║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
-║╔══╣╚╝║╔╗║║║╔╗║║ ║║║═╣║═╣ ║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
-║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣ ║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
-╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝ ╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
-       ║║      ╔═╝║                     ╔═╝║
-       ╚╝      ╚══╝                     ╚══╝
-===================================================  
-              `)
-        mainMenu();    
+ ╔═══╗     ╔╗               ╔═╗╔═╗
+ ║╔══╝     ║║               ║║╚╝║║
+ ║╚══╦╗╔╦══╣║╔══╦╗ ╔╦══╦══╗ ║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+ ║╔══╣╚╝║╔╗║║║╔╗║║ ║║║═╣║═╣ ║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+ ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣ ║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+ ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝ ╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+        ║║      ╔═╝║                     ╔═╝║
+        ╚╝      ╚══╝                     ╚══╝
+ ===================================================  
+   `)
+ mainMenu();
+});
+        
+const mainMenu = () => {
+    inquirer
+    .prompt ([
+        {
+        type:"list",
+        name:"options",
+        message:"What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "view all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee's role",
+            "Quit"]      
+    }])
+.then (answer => {
+    const {options} = answer;
+
+    if(options === "View all departments"){
+        viewDepartments();
     }
+    if(options === "View all roles"){
+        viewRoles();
+    }
+    if(options === "View all employees"){
+        viewEmployees();
+    }
+    if(options === "Add a department"){
+      addDepartmentInput();
+    }
+    if(options === "Add a role"){
+      addRoleInput();
+    }
+    if(options === "Add an employee"){
+      addEmployeeInput();
+    }
+    if(options === "Update an employee's role"){
+      updateRoleInput();
+    }
+    if(options === "Quit"){
+      quit();
+      }
+    });
+  };
+
+const viewDepartments = () => {
+
+    return new Promise((res, reject) => {
+        console.log("Viewing all department\n");
+
+    const sqlQuery = `SELECT department.id AS ID, department.name AS Department FROM department`;
+
+        connection.query(sqlQuery, (err,rows)=>{
+            if(err){
+                return reject(err);
+            } 
+            console.table(rows);
+            mainMenu();
+    });
     
-
-const mainMenu = async () => {
-    try {
-        let result = await inquirer.prompt({
-            type:"list",
-            name:"options",
-            message:"What would you like to do?",
-            choices: [
-              "View all departments",
-              "View all roles",
-              "view all employees",
-              "Add a department",
-              "Add a role",
-              "Add an employee",
-              "Update an employee's role",
-              "Quit"]
-        });
-        switch (result.options) {
-            case "View all departments":
-                viewDepartments();
-                break;
-
-            case "View all roles":
-                viewRoles();
-                break;
-
-            case "View all employees":
-                viewEmployees();
-                break;
-
-            case "Add a department":
-                addDepartmentInput();
-                break;
-
-            case "Add a role":
-                addRoleInput();
-                break;
-
-            case "Add an employee":
-                addEmployeeInput();
-                break;
-
-            case "Update an employee's role":
-                updateRoleInput();
-                break;
-            
-            case "Quit":
-                quit();
-                break;  
-        };
-    }
-    catch (err) {
-        console.log(err);
-        mainMenu(); 
-    };
-}
-
-connect();
+    });
+};   
